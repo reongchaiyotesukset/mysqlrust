b169b927889ae66fc0011072a5e569568a68a51f
reongchaiyote <43132533+reongchaiyotesukset@users.noreply.github.com>
2023-01-21 03:32:54 +0000

    21/01/2566

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ extern crate gtk;
 use mysql::*;
 use gtk::prelude::*;
 use mysql::prelude::*;
-
 use gtk::{Button, Entry, Window, WindowType,Layout};
 
 #[derive(Debug, PartialEq, Eq)]
@@ -12,78 +11,39 @@ struct example {
     data: Option<String>,
 }
 
-fn main() {
-    gtk::init();
+fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+	       
+            let url = "mysql://root:cargorust@localhost:3306/testdb";
+			let mut pool = Pool::new(url)?;
+			let mut conn = pool.get_conn()?; 	
+            //let mut conn = Pool::get_conn(&pool)?;
+             
+             //vector
+              let payments = vec![
+				example { id: 1, data: Some("c#".into()) },
+			  ];      
+			  //insert
+			  /*
+			conn.exec_batch(
+				"INSERT INTO example (id,  data)
+				VALUES (:id, :data)",
+				payments.iter().map(|p| params! {
+				"id" => p.id,
+				"data" => &p.data,
+			 })
+			)?;
+			*/
+			//select
+			 let stack = conn
+        .query_map(
+            "SELECT*from example",
+            |(id, data)| {
+                example { id, data }
+            },
+        )?;
+					println!("{:?}",stack[1].data);
+					//println!("{:?}",conn);
 
-    
-    let window = Window::new(WindowType::Toplevel);
-    window.set_title("MySQL Example");
-    window.set_default_size(350, 250);
-
-    let layout = Layout::builder()
-            .margin(20)
-            .margin_bottom(20)
-            .margin_top(20)
-            .margin_start(20)
-            .margin_end(20)
-            .build();
-            
-	let entry = Entry::builder()
-            .placeholder_text("input")
-            .can_default(true)
-            .can_focus(true)           
-            .margin(20)
-            .margin_bottom(20)
-            .margin_top(20)
-            .margin_start(20)
-            .margin_end(20)
-            .build();
-            
-     let save_button = Button::builder()
-            .label("Click1")
-            .margin(60)
-            .margin_bottom(60)
-            .margin_top(60)
-            .margin_start(60)
-            .margin_end(60)
-            .build();
-        
-        layout.add(&entry);
-        layout.add(&save_button);
-        window.add(&layout);
-
-        //test Error
-        let text = entry.text().as_str();
-
-        
-            let url = "mysql://root:password@localhost:3306/testdb";
-			let pool = Pool::new(url);
-            let mut conn = pool.expect("REASON").get_conn();
-                let payments = vec![
-				example { id: 1, data: Some("foo".into()) },
-    ];
-
-   conn.expect("REASON").exec_batch(
-			"INSERT INTO example (id,  data)
-            VALUES (:id, :data)",
-            payments.iter().map(|p| params! {
-            "id" => p.id,
-            "data" => &p.data,
-        })
-    );
-        save_button.connect_clicked(move |_| {
-		//error
-		
-		
-		//endline	
-        
-		});
-    
-    window.show_all();
-    window.connect_delete_event(|_, _| {
-        gtk::main_quit();
-        Inhibit(false)
-    });
-
-    gtk::main();
+      
+              Ok(())
 }
